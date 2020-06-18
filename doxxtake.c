ee0b6036a3efaa53d2d75f8cf1ee167419149e7f
t3rr0rs-end <67117411+t3rr0rs-end@users.noreply.github.com>
2020-06-18 18:28:13 +0100

    Update doxxtake.c

--- doxxtake.c
+++ doxxtake.c
@@ -30,19 +30,23 @@ int main(){
     char targetAddress2_1[50]; // second word for address 2
     char targetAddress3[50]; // town or city
     int targetZipCode;       // zip code
-    char targetAddress4[50]; // state first word
-    char targetAddress4_1[50];
+    char targetAddress4[50]; // state
     
     char targetEmail[50];
-    int targetPhone;
+    char targetPhone[50;
 	
 	
 	// inputting the victim information 
 	
-	printf("\nEnter targets's Name : ");
+	printf("\nEnter targets's First and Last name : ");
 	scanf("%s %s", targetName1, targetName1_1);
 	printf("\nEnter target's age : ");
 	scanf("%d", &targetAge);
+	printf("Enter target's Phone : ");
+	scanf("%s", targetPhone);
+	printf("\n\nEnter target's Email : ");
+	scanf("%s", targetEmail);
+	
 	printf("\n\nEnter target's house / apartment number : ");
 	scanf("%d", &targetAddress1);
 	printf("\nEnter target's street : ");
@@ -52,11 +56,7 @@ int main(){
 	printf("\nEnter target's Zip Code : ");
 	scanf("%d", &targetZipCode);
 	printf("\nEnter target's state : ");
-	scanf("%s %s", targetAddress4, targetAddress4_1);
-	printf("\n\nEnter target's Email : ");
-	scanf("%s", targetEmail);
-	printf("Enter target's Phone : ");
-	scanf("%d", &targetPhone);
+	scanf("%s", targetAddress4);
 	
 	
 	return 0;
