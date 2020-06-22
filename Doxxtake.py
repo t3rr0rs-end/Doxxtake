c58bf5b0094fd74a8b6507aa646df5038123ec44
t3rr0rs-end <67117411+t3rr0rs-end@users.noreply.github.com>
2020-06-22 15:10:03 +0100

    Update Doxxtake.py

--- Doxxtake.py
+++ Doxxtake.py
@@ -9,23 +9,59 @@ print(" #     # #    #   ##     ##     #   ###### #  #   #      ")
 print(" #     # #    #  #  #   #  #    #   #    # #   #  #      ")
 print(" ######   ####  #    # #    #   #   #    # #    # ###### ")
 print("---------------------------------------------------------")
-
 print("\n")
 
 
-# victim information
+# personal information 
 
-targetName = input("Enter target Name : ")
-targetAge = int(input("Enter target Age : "))
-targetEmail = input("Enter target Email : ")
-targetPhone = input(str("Enter target Phone : "))
+name = input("Enter target's full name : ")
+age = int(input("Enter target's age : "))
+email = input("Enter target's email : ")
+phone = input("Enter target's phone no. (format (000)-000-000) : ")
 
+# address
 
-# address 
+address1 = int(input("\nEnter target's house / apartment number : "))
+address1_1 = input("Enter target's street : ")
+address2 = input("Enter target's Town / City : ")
+address3 = int(input("Enter target's Zip Code : "))
+address4 = input("Enter target's State (Initials only) : ")
+address5 = input("Enter target's Country : ")
 
-targetAddr1 = int(input("\nEnter target house/apartment no. : ")) # house/apartment number
-targetAddr2 = input("Enter target street : ") # street
-targetAddr3 = input("Enter target town/city : ") # town/city
-targetAddr4 = int(input("Enter target zip code : ")) # zip code
-targetAddr5 = input("Enter target state : ") # state
+# online 
 
+instagram = input("\nEnter target's Instagram (@ symbol will be added automatically) : " + "@" )
+snapchat = input("Enter target's snapchat : ")
+facebookURL = input("Enter target's facebook profile : ")
+ip = input("Enter target's IP : ")
+isp = input("Enter target's ISP : ")
+
+print(" ")
+
+# save information to file
+
+f = open("output.txt", "w")
+f.write(" \n")
+f.write(f"Name : {name}\n")
+f.write(f"Age : {age}\n")
+f.write(f"Email : {email}\n")
+f.write(f"Phone No. {phone}\n")
+
+# save address to file
+
+f.write(f"\nHouse / Apartment No. : {address1} {address1_1}\n")
+f.write(f"Enter street : {address1_1}\n")
+f.write(f"Town City : {address2}\n")
+f.write(f"Zip Code : {address3}\n")
+f.write(f"State (Initials only!) : {address4}\n")
+f.write(f"Country : {address5}\n")
+
+# save internet info to file
+
+f.write(f"\nInstagram (@ will be added automatically) : @{instagram}\n")
+f.write(f"Snapchat : {snapchat}\n")
+f.write(f"Facebook URL : {facebookURL}\n")
+f.write(f"IP : {ip}\n")
+f.write(f"ISP : {isp}")
+
+f.close()
